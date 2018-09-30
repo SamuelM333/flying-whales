141f7dd1cf9d74f39d31a6c8a08a2283a52c5779
Samuel Murillo <samuelmurillo333@gmail.com>
2018-09-30 11:23:54 -0300

    Fix datetime search

--- static/js/container.js
+++ static/js/container.js
@@ -1,19 +1,19 @@
-$(document).ready(() => {
+$(document).ready(function () {
     $('.datepicker').datepicker({ autoClose: true, maxDate: new Date() });
     $('.timepicker').timepicker({ autoClose: true });
     $('.modal').modal();
 });
 
-$("#date-start").change(() => {
+$("#date-start").change(function () {
     let date_start = M.Datepicker.getInstance($("#date-start")[0]).date;
     $('#date-end').datepicker({ autoClose: true, minDate: date_start, maxDate: new Date() });
 });
 
-$("#timerange").change(() => {
+$("#timerange").change(function () {
     this.checked ? $(".timerange").removeClass("hide") : $(".timerange").addClass("hide");
 });
 
-$('#log_form').submit(() => {
+$('#log_form').submit(function () {
     // TODO Why [0] ? $(#) should select just one.
     // TODO Add time to date range validation (same day validation)
     let date_start = M.Datepicker.getInstance($("#date-start")[0]).date;
