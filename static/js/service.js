70829dd1525b7cfbb539651aa97198524b018ad7
Samuel Murillo <samuelmurillo333@gmail.com>
2019-01-18 15:00:11 -0300

    Scroll Down log snippet on load

--- static/js/service.js
+++ static/js/service.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
+    let log_snippet = document.getElementById("log-snippet");
+    log_snippet.scrollTo(0, log_snippet.scrollHeight);
     $('.datepicker').datepicker({autoClose: true, maxDate: new Date()});
     $('.timepicker').timepicker({autoClose: true});
     $('.modal').modal();
     let service_id = document.getElementById("service-name").textContent;
     console.log(service_id);
     $.ajax({url: `/service/${service_id}/download-logs/short/50`, success: function(result) {
-        console.log(result.log_lines);
         document.getElementById("log-snippet").innerHTML = result.log_lines;
     }});
 });
