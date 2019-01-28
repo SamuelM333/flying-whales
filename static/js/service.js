f09ecc930909220f9d45759fca8e1f53f0845448
Samuel Murillo <smurillo@asap-consulting.com.ar>
2019-01-28 16:41:01 -0300

    Scroll Down log snippet on load

--- static/js/service.js
+++ static/js/service.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     let log_snippet = document.getElementById("log-snippet");
-    log_snippet.scrollTo(0, log_snippet.scrollHeight);
     $('.datepicker').datepicker({autoClose: true, maxDate: new Date()});
     $('.timepicker').timepicker({autoClose: true});
     $('.modal').modal();
     let service_id = document.getElementById("service-name").textContent;
     console.log(service_id);
     $.ajax({url: `/service/${service_id}/download-logs/short/50`, success: function(result) {
-        document.getElementById("log-snippet").innerHTML = result.log_lines;
+        log_snippet.innerHTML = result.log_lines;
+        log_snippet.scrollTo(0, log_snippet.scrollHeight);
     }});
 });
 
