1afdfb9347f71301cd4ad8156a11f544b98cd483
Samuel Murillo <smurillo@asap-consulting.com.ar>
2019-04-08 18:27:31 -0300

    Log service in js

--- app.py
+++ app.py
@@ -24,10 +24,13 @@ __version__ = "0.2.0"
 
 NODE_RE = re.compile("com\.docker\.swarm\.node\.id=(\w+)")
 
+async_mode = "eventlet"
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = FLASK_SECRET_KEY
-socketio = SocketIO(app)
-thread = None
+socketio = SocketIO(app, async_mode=async_mode)
+# thread = None
+threads = []
 thread_lock = Lock()
 
 LOGFILE = "{}/{}-FlyingWhales.log".format(LOG_DIR, HOST_NAME)
@@ -183,24 +186,62 @@ def service_details(service_id):
         abort(404)
 
 
-def background_thread():
+def background_thread(service):
     """Example of how to send server generated events to clients."""
-    count = 0
-    while True:
-        socketio.sleep(10)
-        count += 1
-        socketio.emit('my_response',
-                      {'data': 'Server generated event', 'count': count},
-                      namespace='/test')
+    app.logger.info("background_thread")
+    log_lines = service.logs(
+        details=True,
+        timestamps=True,
+        stdout=True,
+        stderr=True,
+        follow=True,
+        tail=1
+    )
+
+    for line in log_lines:
+        # TODO use add_node_hostname_to_logs?
+        line = line.decode('utf-8')
+        node_id = NODE_RE.findall(line)
+        try:
+            node_id = node_id[0]
+            try:
+                node_name = nodes[node_id]
+            except KeyError:
+                node_name = docker_client.nodes.get(node_id).attrs["Description"]["Hostname"]
+                nodes[node_id] = node_name
+        except IndexError:
+            node_name = ""
+
+        line = re.sub(r"com\.docker\.swarm\.node\.id=\w+\,", node_name, line)
+        line = re.sub(r"com\.docker\.swarm\.service\.id=\w+\,", "", line)
+        line = re.sub(r"com\.docker\.swarm\.task\.id=\w+", "", line)
+
+        socketio.sleep(0.5)
+        socketio.emit(
+            'log_line',
+            line,
+            namespace='/test'
+        )
 
 
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    global thread
-    with thread_lock:
-        if thread is None:
-            thread = socketio.start_background_task(background_thread)
-    socketio.emit('my_response', {'data': 'Connected', 'count': 0})
+@socketio.on('follow_logs', namespace='/test')
+def follow_logs(data):
+    # https://github.com/miguelgrinberg/Flask-SocketIO/blob/master/example/app.py
+    # TODO Auth https://flask-socketio.readthedocs.io/en/latest/#using-flask-login-with-flask-socketio
+    # TODO How to create a thread per service?
+    # TODO Use rooms for services
+    global threads
+    if current_user.is_authenticated:
+        app.logger.info("follow_logs")
+        app.logger.info(data)
+        service = docker_client.services.get(data)
+        with thread_lock:
+            # if thread is None:
+            thread = socketio.start_background_task(background_thread, service=service)
+            threads.append(thread)
+        socketio.emit('my_response', {'data': 'Connected', 'count': 0})
+    else:
+        return False
 
 
 @app.route('/service/<service_id>/download-logs/short/<log_lines_num>', methods=['GET'])
--- static/js/service.js
+++ static/js/service.js
@@ -1,9 +1,11 @@
+let service_id;
+
 $(document).ready(function () {
     let log_snippet = document.getElementById("log-snippet");
     $('.datepicker').datepicker({autoClose: true, maxDate: new Date()});
     $('.timepicker').timepicker({autoClose: true});
     $('.modal').modal();
-    let service_id = document.getElementById("service-name").textContent;
+    service_id = document.getElementById("service-name").textContent;
     console.log(service_id);
     $.ajax({url: `/service/${service_id}/download-logs/short/50`, success: function(result) {
         log_snippet.innerHTML = result.log_lines;
@@ -19,9 +21,9 @@ var namespace = '/test';
 var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
 
 socket.on('connect', function() {
-    socket.emit('my_event', {data: 'I\'m connected!'});
+    socket.emit('follow_logs', service_id);
 });
 
-socket.on('my_response', function(msg) {
-    console.log(msg);
+socket.on('log_line', function(line) {
+    console.log(line);
 });
